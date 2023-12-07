1d0aa490922e9387f8c3f3a90559489cb04b70b8
pratik kagale <pratikkagale3333@gmail.com>
2023-12-07 21:20:37 +0530

    datatypes of java script

---
+++ Java Script/first/3.js
@@ -0,0 +1,8 @@
+
+let name="pratik";
+
+let r=name.length;
+console.log("Length of name is " + r);
+console.log(name.length-1);
+
+console.log(name[name.length-1]);
---
+++ Java Script/first/4.js
@@ -0,0 +1,8 @@
+
+//premitive data type
+
+let age=22;
+let firstName="pratik";
+
+console.log(typeof age);
+console.log(typeof firstName);
