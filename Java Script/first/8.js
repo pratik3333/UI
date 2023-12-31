a3f36c71d6bb67d0b01eb4de762a2f7cead5e28d
pratik kagale <pratikkagale3333@gmail.com>
2023-12-31 17:57:08 +0530

    datatypes in javascript

---
+++ Java Script/first/8.js
@@ -0,0 +1,9 @@
+
+let a="8"; //datatype is string
+let b=8; //datatype is number
+
+console.log(a==b);
+
+//and our answer is true because here, 
+// == check only values not datatypes so our output will be true
+// and if we change the a's value then our output will be false.
