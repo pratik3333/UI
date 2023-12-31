a06e0d94470d7b274836db29761ce5b66b6a73c7
pratik kagale <pratikkagale3333@gmail.com>
2023-12-31 18:25:02 +0530

    even numbers upto that given number

---
+++ Java Script/first/even_odd.js
@@ -0,0 +1,22 @@
+
+
+let num=22;
+
+if (num % 2 ==0) {
+    console.log("number is even");
+    
+}
+else {
+    console.log("number is odd");
+}
+
+let a=1;
+
+while (a !== num) {
+    
+    if (a %2 == 0) {
+        
+        console.log(a);
+    }
+    a++;
+}
