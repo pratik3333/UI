fb2526ef58154ea75269b70d4d6cd0e92cfd11de
pratik kagale <pratikkagale3333@gmail.com>
2024-01-02 23:24:41 +0530

    Array concatenate

---
+++ Java Script/Array Concatenate/1.js
@@ -0,0 +1,13 @@
+
+
+
+let array1=["item1", "item2", "item3"];
+
+let array2=array1.slice(0); // array concatenate using slice 0 means starting point is 0.
+
+// let array2=[].concat(array1); //array concatenate using concat
+
+console.log(array1===array2);
+
+console.log(array1);
+console.log(array2);
