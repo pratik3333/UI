6a2535fe63e138fc57570a67cdc803f00e0a5d91
pratik kagale <pratikkagale3333@gmail.com>
2024-01-05 18:00:58 +0530

    for in loop

--- Java Script/for in loop/1.js
+++ Java Script/for in loop/1.js
@@ -3,10 +3,15 @@
 
 const fruits = ["Apple","Mango","Banana"];
 
+const fruits2=[];
 for (const index in fruits) {
     
     // console.log(index);  //O/P:- 0 1 2
 
     console.log(fruits[index]); //O/P:- Apple Mango Banana
+
+    fruits2.push(fruits[index].toLocaleUpperCase());
 }
 
+
+console.log(fruits2);
