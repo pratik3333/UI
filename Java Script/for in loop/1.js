a66a109089f8adf1a4f4da88a8c74b9782fde127
pratik kagale <pratikkagale3333@gmail.com>
2024-01-05 17:53:46 +0530

    for in loop

---
+++ Java Script/for in loop/1.js
@@ -0,0 +1,12 @@
+
+//This for in loop give us index
+
+const fruits = ["Apple","Mango","Banana"];
+
+for (const index in fruits) {
+    
+    // console.log(index);  //O/P:- 0 1 2
+
+    console.log(fruits[index]); //O/P:- Apple Mango Banana
+}
+
