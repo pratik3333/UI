42188fa7711144803cb3401a6b53c65bc1030aab
pratik kagale <pratikkagale3333@gmail.com>
2024-01-06 21:51:56 +0530

    key value pair

--- Java Script/objects/1.js
+++ Java Script/objects/1.js
@@ -1,8 +1,18 @@
 
 
 
-const person={name:"Pratik" ,age:25};
+const person={name:"Pratik" ,age:25,
+     "person hobies":["cricket", "swimming"]
+};
 
 console.log(person);
 console.log(typeof(person));// type of a person is Object
+console.log("The name of person is " + person.name);
+console.log("The age of person is " + person.age);
 
+// person.gender("male");
+
+// console.log(person.gender());
+// person.hobbies(["playing","swimming"]);
+
+console.log(person["person hobies"]);
