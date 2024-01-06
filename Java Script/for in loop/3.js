114f86b9e5a3de6ce3976dac48497ddc80333042
pratik kagale <pratikkagale3333@gmail.com>
2024-01-06 23:33:12 +0530

    feacthing objects values from for in loop

---
+++ Java Script/for in loop/3.js
@@ -0,0 +1,18 @@
+
+
+const person={
+    name:"Pratik",
+    age:25,
+    "person hobbies":["riding","walking","sleeping"]
+}
+
+for (let key in person)
+{
+    console.log(person[key]);  //insted of [] if we put there only dot . then our output will be undefined for three times
+    
+}
+
+// O/P:
+// Pratik
+// 25
+// [ 'riding', 'walking', 'sleeping' ]
