982217a4da0386c736cbbeb3125dfde976c615b9
pratik kagale <pratikkagale3333@gmail.com>
2024-01-23 00:08:42 +0530

    sum of two numbers using function

--- Java Script/function/sum.js
+++ Java Script/function/sum.js
@@ -5,6 +5,7 @@ function sum(number1, number2)
     return number1+number2;
 }
 
-const returnValued=sum(3,3);
+const returnValued=sum(3,3); //if we not pass any value then our output will be NaN(not a number)
+                            //  means undefined + undefined = NaN
 
 console.log(returnValued);
